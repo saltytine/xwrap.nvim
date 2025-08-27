4b8d1592e934a149717466e7454d9a3b04ed3138
Saltine <156854448+saltytine@users.noreply.github.com>
2025-08-27 10:07:00 -0400

    i sure hope this works

--- plugin/xwrap.lua
+++ plugin/xwrap.lua
@@ -1,96 +1,75 @@
 local M = {}
 
-M.pairs = {
-  ['"']  = { '"', '"' },
-  ["'"]  = { "'", "'" },
-  ["("]  = { "(", ")" },
-  ["["]  = { "[", "]" },
-  ["{"]  = { "{", "}" },
-  ["<"]  = { "<", ">" },
+local pairs = {
+  ['"'] = '"',
+  ["'"] = "'",
+  ["("] = ")",
+  ["["] = "]",
+  ["{"] = "}",
+  ["<"] = ">"
 }
 
-M.last_action = nil
+function M.wrap(type, char)
+  local opener = char
+  local closer = pairs[char]
+  if not closer then return end
 
-local function get_visual_selection()
-  local bufnr = vim.api.nvim_get_current_buf()
-
-  local vis_mode = vim.fn.mode()
-
-  local start_pos = vim.fn.getpos("'<")
-  local end_pos   = vim.fn.getpos("'>")
-
-  local start_row, start_col = start_pos[2]-1, start_pos[3]-1
-  local end_row, end_col     = end_pos[2]-1, end_pos[3]-1
-
-  if end_row < start_row or (end_row == start_row and end_col < start_col) then
-    start_row, end_row = end_row, start_row
-    start_col, end_col = end_col, start_col
-  end
-
-  if vis_mode == "v" then
-    end_col = end_col + 1
-  elseif vis_mode == "V" then
-    start_col = 0
-    end_row   = end_row + 1
-    end_col   = 0
-  else
-    vim.notify("Blockwise visual mode not supported", vim.log.levels.WARN)
-    return nil, nil
-  end
-
-  local lines = vim.api.nvim_buf_get_text(bufnr, start_row, start_col, end_row, end_col, {})
-  return table.concat(lines, "\n"), {start_row, start_col, end_row, end_col}
-end
-
-local function set_visual_selection(new_text, range)
-  local bufnr = vim.api.nvim_get_current_buf()
-  vim.api.nvim_buf_set_text(bufnr, range[1], range[2], range[3], range[4], vim.split(new_text, "\n"))
-end
-
-local function toggle_wrap(key)
-  local pair = M.pairs[key]
-  if not pair then return end
-  local left, right = pair[1], pair[2]
-
-  local text, range = get_visual_selection()
-  if not text then return end
-
-  local new_text
-  if vim.startswith(text, left) and vim.endswith(text, right) then
-    new_text = text:sub(#left+1, -#right-1)
-    M.last_action = { action="unwrap", key=key }
-  else
-    new_text = left .. text .. right
-    M.last_action = { action="wrap", key=key }
-  end
-
-  set_visual_selection(new_text, range)
-end
-
-function M.setup(opts)
-  opts = opts or {}
-  if opts.pairs then
-    M.pairs = opts.pairs
-  end
-
-  for key, _ in pairs(M.pairs) do
-    vim.keymap.set("v", key, function()
-      toggle_wrap(key)
-    end, { silent = true, desc = "Toggle wrap with " .. key })
-  end
-
-  vim.keymap.set("n", ".", function()
-    if not M.last_action then
-      return vim.cmd("normal! .")
-    end
-    local la = M.last_action
-    if la.action then
-      vim.cmd("normal! gv")
-      toggle_wrap(la.key)
+  local sel = ""
+  if type == "v" then -- characterwise visual
+    vim.cmd('normal! `[v`]y')
+    sel = vim.fn.getreg('"')
+    if sel:sub(1,1) == opener and sel:sub(-1) == closer then
+      sel = sel:sub(2, -2) -- unwrap
     else
-      vim.cmd("normal! .")
+      sel = opener .. sel .. closer
     end
-  end, { silent = true })
+    vim.fn.setreg('"', sel)
+    vim.cmd('normal! gv"0p')
+  elseif type == "V" then -- linewise visual
+    local start_line = vim.fn.line("'<")
+    local end_line = vim.fn.line("'>")
+    for l = start_line, end_line do
+      local line = vim.fn.getline(l)
+      if line:sub(1,1) == opener and line:sub(-1) == closer then
+        vim.fn.setline(l, line:sub(2, -2))
+      else
+        vim.fn.setline(l, opener .. line .. closer)
+      end
+    end
+  elseif type == "\022" then -- blockwise visual (<C-v>)
+    local start_line = vim.fn.line("'<")
+    local end_line = vim.fn.line("'>")
+    for l = start_line, end_line do
+      local line = vim.fn.getline(l)
+      local s = vim.fn.col("'<")
+      local e = vim.fn.col("'>")
+      local chunk = line:sub(s, e)
+      if chunk:sub(1,1) == opener and chunk:sub(-1) == closer then
+        chunk = chunk:sub(2, -2)
+      else
+        chunk = opener .. chunk .. closer
+      end
+      vim.fn.setline(l, line:sub(1, s-1) .. chunk .. line:sub(e+1))
+    end
+  end
+end
+
+local function make_mapping(char)
+  return function()
+    vim.go.operatorfunc = "v:lua.require'wrap'.wrap"
+    vim.fn.feedkeys("g@" .. char, "n") -- makes it repeatable
+  end
+end
+
+function M.setup()
+  for c, _ in pairs(pairs) do
+    vim.keymap.set("v", c, function()
+      local mode = vim.fn.mode()
+      M.wrap(mode, c)
+      vim.fn['repeat#set'](string.format(":'<,'>lua require'wrap'.wrap('%s','%s')<CR>", mode, c))
+    end, { noremap = true, silent = true })
+  end
 end
 
 return M
+
