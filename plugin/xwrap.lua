b525f6b1b167d9f93256967b42120975a664b78f
Saltine <156854448+saltytine@users.noreply.github.com>
2025-08-27 10:15:20 -0400

    h

--- plugin/xwrap.lua
+++ plugin/xwrap.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-local lua_pairs = pairs
-
 local wrappers = {
   ['"']  = { '"', '"' },
   ["'"]  = { "'", "'" },
@@ -11,47 +9,45 @@ local wrappers = {
   ["<"]  = { "<", ">" },
 }
 
-local function is_wrapped(str, open, close)
-  return vim.startswith(str, open) and vim.endswith(str, close)
+local function is_wrapped(line, open, close)
+  return vim.startswith(line, open) and vim.endswith(line, close)
 end
 
-local function wrap_selection(open, close, mode)
-  vim.cmd('normal! gv"zy')
-  local text = vim.fn.getreg('z')
+local function wrap_range(open, close, mode)
+  local start_pos = vim.fn.getpos("'<")
+  local end_pos   = vim.fn.getpos("'>")
 
-  local new
-  if is_wrapped(text, open, close) then
-    new = text:sub(#open + 1, #text - #close)
-  else
-    if mode == 'V' then -- visual line
-      new = open .. text:gsub('\n$', '') .. close .. '\n'
-    elseif mode == '\22' then -- visual block
-      local lines = vim.split(text, '\n')
-      for i, l in ipairs(lines) do
-        lines[i] = open .. l .. close
-      end
-      new = table.concat(lines, '\n')
-    else
-      new = open .. text .. close
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(bufnr, start_pos[2]-1, end_pos[2], false)
+
+  if mode == 'v' then
+    local s_col = start_pos[3]
+    local e_col = end_pos[3]
+
+    local first = lines[1]
+    local last  = lines[#lines]
+
+    lines[1] = first:sub(1, s_col-1) .. open .. first:sub(s_col)
+    lines[#lines] = lines[#lines]:sub(1, e_col) .. close .. last:sub(e_col+1)
+
+  elseif mode == 'V' then
+    lines[1] = open .. lines[1]
+    lines[#lines] = lines[#lines] .. close
+
+  elseif mode == '\22' then
+    for i,l in ipairs(lines) do
+      lines[i] = open .. l .. close
     end
   end
 
-  vim.fn.setreg('z', new)
-  vim.cmd('normal! gv"zp') -- replace selection
-end
-
-local function make_repeatable(fn)
-  return function()
-    fn()
-    vim.fn['repeat#set']("gv", -1) -- make `.` reapply
-  end
+  vim.api.nvim_buf_set_lines(bufnr, start_pos[2]-1, end_pos[2], false, lines)
 end
 
 function M.setup()
-  for key, pair in lua_pairs(wrappers) do
+  for key, pair in pairs(wrappers) do
     vim.keymap.set('x', key, function()
       local mode = vim.fn.mode()
-      wrap_selection(pair[1], pair[2], mode)
+      wrap_range(pair[1], pair[2], mode)
     end, { noremap = true, silent = true })
   end
 end
